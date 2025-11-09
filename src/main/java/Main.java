7bc8a01e297ed12b1235f097ed497db4fb469bc1
mats <mat.eidelman@gmail.com>
2025-11-10 00:48:15 +0100

    (main) Logger test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import log4Mats.LogLevel;
+import log4Mats.Logger;
+import logging.LoggerProvider;
 
 public class Main {
+
+    static void main() {
+        Logger logger = LoggerProvider.getLogger();
+        logger.setLogToConsole(true);
+
+        logger.log(LogLevel.TRACE, ">> Starting shop");
+
+
+    }
 }
